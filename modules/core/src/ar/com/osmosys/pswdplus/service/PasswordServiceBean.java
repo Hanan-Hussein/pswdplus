bf1079c8b937411fadad7775e2ee576da6d76b23
Ale <arapoport@gmail.com>
2019-04-02 12:59:24 -0300

    check if password exist in password history

--- modules/core/src/ar/com/osmosys/pswdplus/service/PasswordServiceBean.java
+++ modules/core/src/ar/com/osmosys/pswdplus/service/PasswordServiceBean.java
@@ -1,17 +1,39 @@
 package ar.com.osmosys.pswdplus.service;
 
+import com.haulmont.cuba.core.EntityManager;
+import com.haulmont.cuba.core.Persistence;
+import com.haulmont.cuba.core.Query;
+import com.haulmont.cuba.core.Transaction;
 import org.springframework.stereotype.Service;
 
+import javax.inject.Inject;
 import java.util.UUID;
 
 @Service(PasswordService.NAME)
 public class PasswordServiceBean implements PasswordService {
 
 
+    @Inject
+    Persistence persistence;
+
     @Override
     public boolean isPasswordInHistory(UUID userId, String passwordHash)
     {
-        return false;
+        boolean result=false;
+
+        Transaction tx=persistence.createTransaction();
+        EntityManager em=persistence.getEntityManager();
+
+        Query qCount=em.createQuery("select count(ph) from pswd$PasswordHistory ph where ph.user.id=:userId and ph.passwordHash=:passwordHash");
+        qCount.setParameter("userId",userId);
+        qCount.setParameter("passwordHash",passwordHash);
+        long count=(Long) qCount.getSingleResult();
+
+        if(count>0)
+            result=true;
+
+        return result;
+
     }
 
 
