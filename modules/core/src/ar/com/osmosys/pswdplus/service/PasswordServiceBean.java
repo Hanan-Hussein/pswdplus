52977148deeaedd5924d522fe39f01f94c3d4498
Hanan-Hussein <hanhuss035@gmail.com>
2022-11-04 11:09:59 +0300

    ft:used bcrypt to compare

--- modules/core/src/ar/com/osmosys/pswdplus/service/PasswordServiceBean.java
+++ modules/core/src/ar/com/osmosys/pswdplus/service/PasswordServiceBean.java
@@ -4,9 +4,11 @@ import com.haulmont.cuba.core.EntityManager;
 import com.haulmont.cuba.core.Persistence;
 import com.haulmont.cuba.core.Query;
 import com.haulmont.cuba.core.Transaction;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+import java.util.List;
 import java.util.UUID;
 
 @Service(PasswordService.NAME)
@@ -17,26 +19,28 @@ public class PasswordServiceBean implements PasswordService {
     private Persistence persistence;
 
     @Override
-    public boolean isPasswordInHistory(UUID userId, String passwordHash)
-    {
-        boolean result=false;
+    public boolean isPasswordInHistory(UUID userId, String passwordHash) {
+        boolean result = false;
 
-        Transaction tx=persistence.createTransaction();
-        EntityManager em=persistence.getEntityManager();
+        Transaction tx = persistence.createTransaction();
 
-        Query qCount=em.createQuery("select count(ph) from pswdplus$PasswordHistory ph where ph.user.id=:userId and ph.passwordHash=:passwordHash");
-        qCount.setParameter("userId",userId);
-        qCount.setParameter("passwordHash",passwordHash);
-        long count=(Long) qCount.getSingleResult();
+        EntityManager em = persistence.getEntityManager();
+        Query query = em.createQuery("select ph.passwordHash from pswdplus$PasswordHistory ph where ph.user.id=:userId");
 
+        query.setParameter("userId", userId);
+        List<String> passwordhash = query.getResultList();
         tx.commit();
 
-        if(count>0)
-            result=true;
+        long count = 0;
+        if (passwordhash.size() > 0) {
 
+            for (String h : passwordhash) {
+                if (BCrypt.checkpw(passwordHash, h)) // check password on password history
+                    count += 1;
+            }
+            if (count > 0)
+                result = true;
+        }
         return result;
-
     }
-
-
 }
